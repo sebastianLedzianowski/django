d8e7eb20fbdcc176abe6e96a133a0434f6ff8575
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-01-27 16:05:49 +0100

    Add ".env" nad .gitignore

--- quotes/settings.py
+++ quotes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +136,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # E-mail settings
-load_dotenv()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
